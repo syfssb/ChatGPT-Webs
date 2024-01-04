4882ca974f36aab55f40ed466c5a6b7dc254334e
CherryChat <1073634403@qq.com>
2024-01-04 23:20:05 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -42,34 +42,30 @@ onMounted(() => {
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
       <h2 class="text-xl font-bold">
-        Version - {{ pkg.version }}
+       关于此项目
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          {{ $t("setting.openSource") }}
+          此站为我们主站
           <a
             class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
+            href="https://new.cherrychat.org"
             target="_blank"
           >
-            GitHub
+            樱桃茶 GPT
+          </a>的免费体验站。本站功能相对简单粗暴，随来随用，希望大家喜欢。后台使用的是gpt-4模型。由于人数可能很多，也许会出现卡顿的情况。更多OpenAI 最新的模型，更平滑，流畅，稳定，定制化的功能请去咱们的主站。
+          <a
+            class="text-blue-600 dark:text-blue-500"
+            href="https://new.cherrychat.org"
+            target="_blank"
+          >
+            樱桃茶 GPT
           </a>
-          {{ $t("setting.freeMIT") }}
-        </p>
-        <p>
-          {{ $t("setting.stars") }}
-        </p>
+					樱桃茶 vip QQ群：694177779;
+					群内每日随机赠送 10w token 对话币。 群内所有成员享受套餐6 折福利
+🔥600 元，全年 GPT4无限使用套餐，限时出售中！
+				</p>
       </div>
-      <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
-      <p v-if="isChatGPTAPI">
-        {{ $t("setting.monthlyUsage") }}：{{ config?.usage ?? '-' }}
-      </p>
-      <p v-if="!isChatGPTAPI">
-        {{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}
-      </p>
-      <p>{{ $t("setting.timeout") }}：{{ config?.timeoutMs ?? '-' }}</p>
-      <p>{{ $t("setting.socks") }}：{{ config?.socksProxy ?? '-' }}</p>
-      <p>{{ $t("setting.httpsProxy") }}：{{ config?.httpsProxy ?? '-' }}</p>
     </div>
   </NSpin>
 </template>
